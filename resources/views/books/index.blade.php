864b8861d3da169250b83c44651b27e0ed62488a
ChamudikaDeSilva <chamudikadesilva97@gmail.com>
2025-09-30 09:15:30 +0530

    Small Fix

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -32,7 +32,7 @@
                     <td class="border border-gray-200 px-4 py-2">{{ $book->description }}</td>
                     <td class="border border-gray-200 px-4 py-2">{{ $book->category->name ?? 'N/A' }}</td>
                     <td class="border border-gray-200 px-4 py-2">{{ $book->author->name ?? 'N/A' }}</td>
-                    <td class="border border-gray-200 px-4 py-2">{{ $book->published_date }}</td>
+                    <td class="border border-gray-200 px-4 py-2">{{ $book->published_at }}</td>
                     <td class="border border-gray-200 px-4 py-2 text-center flex justify-center space-x-3">
                         {{-- View button --}}
                         <a href="{{ route('books.show', $book->id) }}"
